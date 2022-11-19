4c7bafcd8dd2919a15794ba736e9df4e0bee5e38
amir <amir.kashapov@protonmail.ch>
2022-11-20 03:06:16 +1100

    feat: simple card without styles

--- site/client/src/App.vue
+++ site/client/src/App.vue
@@ -1,8 +1,13 @@
 <template>
-    <div class="flex flex-col grow min-h-screen text-white bg-orange-200">
+    <div class="flex flex-col grow min-h-screen text-white bg-orange-200 mb-10">
         <NavBar />  
-        <div class="flex flex-col mx-10 my-10 border-2 rounded-lg">
+        <div class="flex flex-col mx-10 mt-10 border-2 rounded-lg">
           <ImageCarousel />
+        </div>
+        <div class="flex flex-col wrap mx-10 mt-10 border-2 rounded-lg bg-red-300">
+          <div class="my-10">
+            <SimpleCard header="header text" body="body text" footer="footer text"/>
+          </div>
         </div>
     </div>
 </template>
@@ -10,6 +15,7 @@
 <script setup>
 import NavBar from './components/NavBar.vue';
 import ImageCarousel from './components/ImageCarousel.vue';
+import SimpleCard from './components/SimpleCard.vue';
 </script>
 
 <style scoped>
---
+++ site/client/src/components/SimpleCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex justify-center">
+        <div class="block rounded-lg shadow-lg bg-white max-w-sm text-center">
+            <div class="py-3 px-6 border-b border-gray-300 text-gray-600 text-xl">
+                {{ header }}
+            </div>
+            <div class="p-6">
+            <p class="text-gray-700 text-lg mb-4">
+                {{ body }}
+            </p>
+            </div>
+            <div class="py-3 px-6 border-t border-gray-300 text-gray-600 text-lg">
+                {{ footer }} 
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+  defineProps(['header', 'body', 'footer']);
+</script>
