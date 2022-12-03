5a6a4946aa39bd3de941ae0426480aa4873cd94a
Amir <amir.kashapov@protonmail.ch>
2022-12-03 17:43:06 +1100

    fix: carousel transitions

--- site/client/src/components/ImageCarousel.vue
+++ site/client/src/components/ImageCarousel.vue
@@ -1,56 +1,65 @@
 <template>
 
-  <div id="carouselExampleCaptions" class="carousel slide relative" data-bs-ride="carousel">
+  <div
+    id="carouselDarkVariant"
+    class="carousel slide carousel-fade carousel-dark relative"
+    data-bs-ride="carousel"
+  >
+    <!-- Indicators -->
     <div class="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
       <button
-        type="button"
-        data-bs-target="#carouselExampleCaptions"
+        data-bs-target="#carouselDarkVariant"
         data-bs-slide-to="0"
         class="active"
         aria-current="true"
         aria-label="Slide 1"
       ></button>
       <button
-        type="button"
-        data-bs-target="#carouselExampleCaptions"
+        data-bs-target="#carouselDarkVariant"
         data-bs-slide-to="1"
-        aria-label="Slide 2"
+        aria-label="Slide 1"
       ></button>
       <button
-        type="button"
-        data-bs-target="#carouselExampleCaptions"
+        data-bs-target="#carouselDarkVariant"
         data-bs-slide-to="2"
-        aria-label="Slide 3"
+        aria-label="Slide 1"
       ></button>
     </div>
+
+    <!-- Inner -->
     <div class="carousel-inner relative w-full overflow-hidden">
+      <!-- Single item -->
       <div class="carousel-item active relative float-left w-full">
         <img
           src="../assets/image2.jpg"
-          class="block w-full rounded-lg"
-          alt="..."
+          class="block w-full"
+          alt="Motorbike Smoke"
         />
         <div class="carousel-caption hidden md:block absolute text-center">
-          <h5 class="text-3xl text-sky-600 font-bold">Indigineous Image</h5>
-          <p class="text-xl text-sky-600 font-bold">Some Text</p>
+          <h5 class="text-xl">First slide label</h5>
+          <p>Some representative placeholder content for the first slide.</p>
         </div>
       </div>
+
+      <!-- Single item -->
       <div class="carousel-item relative float-left w-full">
         <img
-          src="https://mdbootstrap.com/img/Photos/Slides/img%20(23).jpg"
+          src="../assets/image2.jpg"
           class="block w-full"
-          alt="..."
+          alt="Mountaintop"
         />
         <div class="carousel-caption hidden md:block absolute text-center">
           <h5 class="text-xl">Second slide label</h5>
           <p>Some representative placeholder content for the second slide.</p>
         </div>
       </div>
+
+      <!-- Single item -->
       <div class="carousel-item relative float-left w-full">
         <img
           src="../assets/image2.jpg"
           class="block w-full"
-          alt="..."
+          alt="Woman Reading a Book"
         />
         <div class="carousel-caption hidden md:block absolute text-center">
           <h5 class="text-xl">Third slide label</h5>
@@ -58,10 +67,13 @@
         </div>
       </div>
     </div>
+    <!-- Inner -->
+
+    <!-- Controls -->
     <button
       class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
       type="button"
-      data-bs-target="#carouselExampleCaptions"
+      data-bs-target="#carouselDarkVariant"
       data-bs-slide="prev"
     >
       <span class="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
@@ -70,7 +82,7 @@
     <button
       class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
       type="button"
-      data-bs-target="#carouselExampleCaptions"
+      data-bs-target="#carouselDarkVariant"
       data-bs-slide="next"
     >
       <span class="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
